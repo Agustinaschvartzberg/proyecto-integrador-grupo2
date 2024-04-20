e315a26b1c4e0326c93aef765df66f86c5f44f89
Agustinaschvartzberg <142595355+Agustinaschvartzberg@users.noreply.github.com>
2024-04-20 19:50:46 -0300

    arreglos en routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-const indexController = require("../controllers/indexController");
+var indexController = require("../controllers/indexController");
 
 router.get('/', indexController.index);
+router.get('/index', indexController.index);
+
 module.exports = router;
+
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+
 var userController = require('../controllers/userController');
 
 /* GET users listing. */
@@ -12,4 +13,4 @@ router.get('/profile', userController.profile);
 router.get('/profile-edit', userController.edit);
 
 module.exports = router;
-g
+
