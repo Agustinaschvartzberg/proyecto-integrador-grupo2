90ce36bde245fbe86cae86a9d40743ff61523046
Agustinaschvartzberg <142595355+Agustinaschvartzberg@users.noreply.github.com>
2024-06-20 20:22:51 -0300

    home page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,21 +2,23 @@ const db= require('../database/models');
 
 const indexController = {
     index: function (req, res) {
-        db.Producto.findAll({include: [ { association: 'usuario' }]})
-
-          .then(function (result) {
-                          
-              return res.render('index', { productos:productos})
-            } 
-          )
+        db.Producto.findAll({
+          include: [ 
+          { association: 'usuario'}, {association: 'comentarios'}
+        ],
+        },
+        {order: [
+          ['created_at', 'DESC']]})
+          .then(function(result) {
+            return res.render('index', {
+              lista: data.lista, comment: data.comment, usuario: data.usuario
+            }
+          )})
           .catch(function (error) {
             console.log(error)
           })
           },
-    }
+        }
 
 module.exports = indexController; 
    
-// search: function(req, res){
- //       res.render('search-results')
- //   }
