b0aaca697aceb450093ef99673c3034fa3c67a3c
Agustinaschvartzberg <142595355+Agustinaschvartzberg@users.noreply.github.com>
2024-06-23 15:15:05 -0300

    cambios en la ruta de user

--- routes/user.js
+++ routes/user.js
@@ -2,15 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 var userController = require('../controllers/userController');
+const registerController = require('../controllers/registerController');
+
 
 router.get('/login', userController.login);
 
-router.get('/register', userController.register);
+router.get('/register', registerController.register);
 
 router.get('/profile', userController.profile);
 
 router.get('/profile-edit', userController.edit);
 
-router.post('/register/store', userController.store);
+router.post('/register/store', registerController.store);
 
 module.exports = router;
