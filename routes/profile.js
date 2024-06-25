3380dac4ba56e1c7cf48972dbcdce4e12b49a5da
Agustinaschvartzberg <142595355+Agustinaschvartzberg@users.noreply.github.com>
2024-06-24 22:40:57 -0300

    cambio en ruta de profile

--- routes/profile.js
+++ routes/profile.js
@@ -3,6 +3,7 @@ let profileController = require('../controllers/profileController');
 var router = express.Router(); 
 
 router.get('/:username', profileController.det);
+router.get('/', profileController.det);
 
 
 
